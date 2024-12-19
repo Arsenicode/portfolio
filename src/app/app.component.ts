10f09a871550d856f0502622891e6510fba6a1cf
Elio Francis <eliofrancis2003@gmail.com>
2024-12-19 18:11:03 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'portfolio';
+
+  downloadCV() {
+    const link = document.createElement('a');
+    link.href = 'assets/ElioFrancis.pdf';  // Path relative to the 'src' folder
+    link.download = 'ElioFrancis.pdf';
+    link.click();
+  }
 }
